014dd2842058eed14f23179c852bfdec23de4a8d
mateus <22.01082-3@maua.br>
2024-11-06 19:29:38 -0300

    feat: add create_course route

---
+++ src/modules/create_course/app/create_course_presenter.py
@@ -0,0 +1,17 @@
+from .create_course_controller import CreateCourseController
+from .create_course_usecase import CreateCourseUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+repo = Environments.get_course_repo()()
+usecase = CreateCourseUsecase(repo)
+controller = CreateCourseController(usecase)
+
+def lambda_handler(event, context):
+    
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    response = httpResponse.toDict()
+    
+    return response
