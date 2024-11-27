056478bae65e2ae460c15c48fc41425dcfa8a9a4
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2024-11-26 23:26:25 -0300

    Update iac_stack.py

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -1,17 +1,18 @@
 from aws_cdk import Stack
+from aws_cdk.aws_apigateway import RestApi, Cors, TokenAuthorizer
 from constructs import Construct
-from aws_cdk.aws_apigateway import RestApi, Cors, CfnAuthorizer
-from .dynamo_stack import DynamoStack
-from .bucket_stack import BucketStack
-from .lambda_stack import LambdaStack
+import os
 
 
 class IacStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        self.dynamo_stack = DynamoStack(self)
-        self.bucket_stack = BucketStack(self)
+        tenant_id = os.environ.get("AZURE_AD_TENANT_ID")
+        client_id = os.environ.get("AZURE_AD_CLIENT_ID")
+
+        if not tenant_id or not client_id:
+            raise ValueError("AZURE_AD_TENANT_ID and AZURE_AD_CLIENT_ID must be provided!")
 
         self.rest_api = RestApi(
             self,
@@ -25,33 +26,27 @@ class IacStack(Stack):
             },
         )
 
-        azure_ad_oidc_provider_arn = f"arn:aws:apigateway:{Stack.of(self).region}:oidc-provider/login.microsoftonline.com"
-
-        self.azure_ad_authorizer = CfnAuthorizer(
+        self.azure_ad_authorizer = TokenAuthorizer(
             self,
             "AzureADAuthorizer",
-            name="AzureAD_OIDC",
-            rest_api_id=self.rest_api.rest_api_id,
-            type="JWT",
+            handler=self.create_validation_lambda(client_id, tenant_id),
             identity_source="method.request.header.Authorization",
-            provider_arns=[azure_ad_oidc_provider_arn],
         )
 
-        ENVIRONMENT_VARIABLES = {
-            "STAGE": "TEST",
-            "AZURE_AD_CLIENT_ID": "{client_id}",
-            "AZURE_AD_TENANT_ID": "{tenant_id}",
-            "DYNAMO_TABLE_COURSE": self.dynamo_stack.dynamo_table_course.table_name,
-            "DYNAMO_TABLE_EVENT": self.dynamo_stack.dynamo_table_event.table_name,
-            "DYNAMO_TABLE_MEMBER": self.dynamo_stack.dynamo_table_member.table_name,
-            "DYNAMO_TABLE_STU_ORG": self.dynamo_stack.dynamo_table_student_org.table_name,
-        }
+    def create_validation_lambda(self, client_id: str, tenant_id: str):
+        """
+        Cria uma função Lambda para validar o token JWT do Azure AD.
+        """
+        from aws_cdk import aws_lambda as lambda_
 
-        self.lambda_stack = LambdaStack(self, environment_variables=ENVIRONMENT_VARIABLES)
-
-        # Grant DynamoDB permissions to Lambda functions
-        for f in self.lambda_stack.functions_that_need_dynamo_permissions:
-            self.dynamo_stack.dynamo_table_course.grant_read_write_data(f)
-            self.dynamo_stack.dynamo_table_event.grant_read_write_data(f)
-            self.dynamo_stack.dynamo_table_member.grant_read_write_data(f)
-            self.dynamo_stack.dynamo_table_student_org.grant_read_write_data(f)
+        return lambda_.Function(
+            self,
+            "ValidateTokenLambda",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            handler="validate_token.handler",
+            code=lambda_.Code.from_asset("src/validate_token"),
+            environment={
+                "AZURE_AD_CLIENT_ID": client_id,
+                "AZURE_AD_TENANT_ID": tenant_id,
+            },
+        )
