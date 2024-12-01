fd7897890dac31c0f08cf111fec15cf3a0358a22
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2024-12-01 01:26:16 -0300

    fix: package missing

--- iac/adjust_layer_directory.py
+++ iac/adjust_layer_directory.py
@@ -34,6 +34,7 @@ def adjust_layer_directory(shared_dir_name: str, destination: str):
     python_lib_path = os.path.join(destination_directory, "python")
     os.makedirs(python_lib_path, exist_ok=True)
     subprocess.check_call([sys.executable, "-m", "pip", "install", "pyjwt", "-t", python_lib_path])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests", "-t", python_lib_path])
 
 
 if __name__ == '__main__':
