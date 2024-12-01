ea7cd9e4f7159d9d7373635149b898a589511432
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2024-12-01 01:22:59 -0300

    fix: lambda version compability

--- src/shared/helpers/auth.py
+++ src/shared/helpers/auth.py
@@ -1,25 +1,17 @@
 import os
-from msal import ConfidentialClientApplication
+import requests
 from jwt import decode, DecodeError, ExpiredSignatureError
 from jwt.algorithms import RSAAlgorithm
 
 def authenticate(event):
     client_id = os.environ.get("CLIENT_ID")
-    client_secret = os.environ.get("CLIENT_SECRET")
     authority = os.environ.get("AUTHORITY")
-
-    # Instancia o cliente MSAL
-    app = ConfidentialClientApplication(
-        client_id,
-        authority=authority,
-        client_credential=client_secret,
-    )
 
     # Obtém o token do cabeçalho de autorização
     token = None
     if "Authorization" in event["headers"]:
         auth_header = event["headers"]["Authorization"]
-        if auth_header.startswith("Bearer "):
+        if (auth_header.startswith("Bearer ")):
             token = auth_header.split(" ")[1]
 
     if not token:
@@ -27,7 +19,7 @@ def authenticate(event):
 
     try:
         jwks_uri = f"{authority}/discovery/v2.0/keys"
-        jwks = app.http_client.get(jwks_uri).json()
+        jwks = requests.get(jwks_uri).json()
         key = RSAAlgorithm.from_jwk(jwks["keys"][0])
 
         # Decodifica e valida o token
