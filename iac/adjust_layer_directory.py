bfc6e1e743536138433b7acb8f509d2312ffd048
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2024-12-01 02:08:34 -0300

    Update adjust_layer_directory.py

--- iac/adjust_layer_directory.py
+++ iac/adjust_layer_directory.py
@@ -37,8 +37,8 @@ def adjust_layer_directory(shared_dir_name: str, destination: str):
     subprocess.check_call([sys.executable, "-m", "pip", "install", "requests", "-t", python_lib_path])
     subprocess.check_call([
         sys.executable, 
-        "-m", "pip", "install", "cryptography", 
-        "--platform", "manylinux2010_x86_64", 
+        "-m", "pip", "install", "cryptography==3.3.2",
+        "--platform", "manylinux2010_x86_64",
         "--implementation", "cp",
         "--only-binary=:all:", 
         "--upgrade", 
