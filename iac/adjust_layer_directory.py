95b8e1998216e943583059741d5e82e1c35bb2a9
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2024-12-01 00:52:26 -0300

    Update adjust_layer_directory.py

--- iac/adjust_layer_directory.py
+++ iac/adjust_layer_directory.py
@@ -34,8 +34,7 @@ def adjust_layer_directory(shared_dir_name: str, destination: str):
     python_lib_path = os.path.join(destination_directory, "python")
     os.makedirs(python_lib_path, exist_ok=True)
     subprocess.check_call([sys.executable, "-m", "pip", "install", "msal", "-t", python_lib_path])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyjwt", "-t", python_lib_path])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "cryptography", "-t", python_lib_path])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "snowflake-connector-python==2.7.9", "-t", python_lib_path])
 
 
 if __name__ == '__main__':
