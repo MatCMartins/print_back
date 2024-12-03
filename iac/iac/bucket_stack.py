62c2994f0d9ffd04ab4bbfa95d95cd8e3805e048
mateus <22.01082-3@maua.br>
2024-12-02 23:57:40 -0300

    feat: remove bucket policies

--- iac/iac/bucket_stack.py
+++ iac/iac/bucket_stack.py
@@ -14,29 +14,58 @@ class BucketStack(Construct):
             "CourseBucket",
             versioned=True,
             removal_policy=RemovalPolicy.DESTROY,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False,
+            ),
         )
         self.event_bucket = s3.Bucket(
             self,
             "EventBucket",
             versioned=True,
             removal_policy=RemovalPolicy.DESTROY,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False,
+            ),
         )
         self.member_bucket = s3.Bucket(
             self,
             "MemberBucket",
             versioned=True,
             removal_policy=RemovalPolicy.DESTROY,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False,
+            ),
         )
         self.student_org_bucket = s3.Bucket(
             self,
             "StudentOrganizationBucket",
             versioned=True,
             removal_policy=RemovalPolicy.DESTROY,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False,
+            ),
         )
         self.notification_bucket = s3.Bucket(
             self,
             "NotificationBucket",
             versioned=True,
             removal_policy=RemovalPolicy.DESTROY,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False,
+            ),
         )
-
